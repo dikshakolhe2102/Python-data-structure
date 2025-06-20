a8dbecfd52ed0819749e51cb11f2033368666004
dikshakolhe2102 <dikshakolhe54@gmail.com>
2025-06-20 11:39:50 +0530

    frozen set

---
+++ frozen set.py
@@ -0,0 +1,15 @@
+# s={1,2,3,4,5}
+# f=frozenset(s)
+# print(f)
+# print(type(f))
+# #f.add(10)
+# #nested set or frozen set
+# s.add(f)
+# print(s)
+
+# f1=frozenset("python")
+# print(f1)
+
+f=frozenset([10,20,30])
+print(f)
+
